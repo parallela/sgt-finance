e5a3d24aa7ec187b92d4a7aece89e227bb243568
lubomirstankov <lubomir.c.stankov@gmail.com>
2023-10-07 23:03:57 +0300

    SGT-Finance: Adding API communicator

--- app/Providers/SgtFinanceServiceProvider.php
+++ app/Providers/SgtFinanceServiceProvider.php
@@ -2,18 +2,44 @@
 
 namespace App\Providers;
 
+use App\Clients\Polygon;
+use App\Services\ScrapperService;
 use App\Services\WorthService;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class SgtFinanceServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(WorthService::class, fn () => new WorthService());
+        /**
+         * Bind a singleton of WorthService that will be corresponded for saving the data into the database and influx storage
+         */
+        $this->app->singleton(WorthService::class, fn() => new WorthService());
+
+        /**
+         * Bind a singleton of ScrapperService that will be corresponded for scrapping
+         */
+        $this->app->singleton(
+            ScrapperService::class,
+            fn() => new ScrapperService(new Polygon())
+        );
     }
 
+    /**
+     * Here should be all the custom bootable singleton providers, macros
+     */
     public function boot(): void
     {
-        //Here should be all the custom bootable singleton providers
+        // Add http provider for communicating the polygon api
+        Http::macro(
+            'polygon',
+            fn() => Http::withHeaders([
+                'Authorization' =>
+                    'Bearer ' . config('clients.polygon.api_key'), // Providing API key for the requests
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ])
+        );
     }
 }
---
+++ config/clients/polygon.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    /**
+     * The api key that will be corresponded for getting a data from the api
+     */
+    'api_key' => env('POLYGON_API_KEY', '')
+];
